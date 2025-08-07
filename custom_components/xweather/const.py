37ab6f0ff2cea02f9630e0c9a02553e81516c2df
Thomas B. Clark <git@ferree-clark.org>
2025-08-07 10:56:19 -0700

    add more weather constants

--- custom_components/xweather/const.py
+++ custom_components/xweather/const.py
@@ -15,7 +15,7 @@ API_BASE = "https://data.api.xweather.com"
 
 # Map XWeather coded conditions to Home Assistant weather conditions/icons
 ICON_MAP = {
-    # Cloud-only codes
+    # Cloud codes
     "CL": "sunny",
     "CL-N": "clear-night",
     "FW": "partlycloudy",
@@ -23,18 +23,38 @@ ICON_MAP = {
     "SC-N": "partlycloudy-night",
     "BK": "cloudy",
     "OV": "cloudy",
+
     # Precipitation and weather codes
     "R": "rainy",
     "RW": "rainy",
+    "LD": "rainy",  # Drizzle is a form of rain
     "RS": "snowy-rainy",
     "S": "snowy",
     "SW": "snowy",
-    "T": "lightning",
+    "SS": "snowy",  # Snow/sleet mix is similar to snowy-rainy
+    "SI": "snowy-rainy",
     "WM": "snowy-rainy",
-    "ZR": "pouring",
-    "ZL": "pouring",
+    "IP": "snowy-rainy",  # Ice pellets/sleet
+    "T": "lightning-rainy",  # Thunderstorms always imply rain
+    "TOT": "exceptional",  # Tornado
+    "FC": "exceptional",  # Funnel Cloud
+    "WP": "exceptional",  # Waterspouts
+    "AH": "hail",
+    "ZR": "pouring",  # Freezing rain is similar to pouring rain
+    "ZL": "pouring",  # Freezing drizzle
     "ZF": "fog",
     "F": "fog",
-    "H": "fog",
-    "BR": "fog",
+    "FR": "fog",  # Frost can be represented as fog
+    "IF": "fog",  # Ice fog
+    "BR": "fog",  # Mist is a form of fog
+    "H": "fog",  # Haze is a form of fog
+    "BS": "windy-variant",  # Blowing snow
+    "BD": "windy-variant",  # Blowing dust
+    "BN": "windy-variant",  # Blowing sand
+    "BY": "windy-variant",  # Blowing spray
+    "K": "fog",  # Smoke
+    "VA": "exceptional",  # Volcanic ash
+    "IC": "snowy",  # Ice crystals
+    "UP": "exceptional",  # Unknown precipitation
+    "ZY": "exceptional"  # Freezing spray
 }
